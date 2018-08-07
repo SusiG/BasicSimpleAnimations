d9c5e5c671466b733dca3a28daf52e118998b2b3
Bhagya <bhagyalakshmi.guntamadugu@gmail.com>
2018-08-07 15:04:52 +0530

    Removed Print

--- BasicSimpleAnimations/ViewController.swift
+++ BasicSimpleAnimations/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("BasicSimpleAnimations")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
