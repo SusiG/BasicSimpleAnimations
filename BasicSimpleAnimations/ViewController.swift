c2034087e991c8f4b18e2ee2af34cb3d1be3424a
Bhagya <bhagyalakshmi.guntamadugu@gmail.com>
2018-08-07 15:49:33 +0530

    Applied transition animation to UIView

--- BasicSimpleAnimations/ViewController.swift
+++ BasicSimpleAnimations/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var animatedView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.initalAnimation()
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,6 +25,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initalAnimation(){
+        animatedView.transform = CGAffineTransform.init(scaleX: 0.2, y: 0.2)
+       
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.animatedView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+//
+//        }) { (iscompleted) in
+//            
+//            self.animatedView.transform = .identity
+//        }
+//
+        
+        UIView.transition(with: animatedView, duration: 1.0, options: UIViewAnimationOptions.transitionCurlUp, animations: {
+            self.animatedView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+
+        }) { (iscompleted) in
+            self.animatedView.transform = .identity
+        }
+        
     }
 
 
