2bdb54680fc2c110bf290bb1d0494b3059e2dbde
Bhagya <bhagyalakshmi.guntamadugu@gmail.com>
2018-08-07 15:59:04 +0530

    Transition Animation

---
+++ BasicSimpleAnimations/TransitionAnimations.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimations.swift
+//  BasicSimpleAnimations
+//
+//  Created by Bhagya on 07/08/18.
+//  Copyright © 2018 Bhagya. All rights reserved.
+//
+
+import UIKit
+
+class TransitionAnimations: NSObject {
+
+}
