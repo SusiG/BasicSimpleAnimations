8ec7c966c476c7d610ac39b2b63f458345df6a88
Bhagya <bhagyalakshmi.guntamadugu@gmail.com>
2018-08-07 14:59:02 +0530

    Initial App commit

---
+++ BasicSimpleAnimations/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BasicSimpleAnimations
+//
+//  Created by Bhagya on 07/08/18.
+//  Copyright © 2018 Bhagya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("BasicSimpleAnimations")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
